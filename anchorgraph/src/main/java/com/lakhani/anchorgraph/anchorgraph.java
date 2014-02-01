1e0d0a6f4e8ec84ed4ce0d3b3e6fa496b5bf579c
Chirag Lakhani <chirag.lakhani@gmail.com>
2014-02-01 17:45:52 -0500

    anchorgraph
    test anchorgraph code where it simply parses a csv file and writes to
    hdfs

--- anchorgraph/src/main/java/com/lakhani/anchorgraph/anchorgraph.java
+++ anchorgraph/src/main/java/com/lakhani/anchorgraph/anchorgraph.java
@@ -13,52 +13,51 @@ package com.lakhani.anchorgraph;
   import java.io.IOException;
   import java.util.*;
            
-  import org.apache.hadoop.fs.Path;
   import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.Path;
   import org.apache.hadoop.io.*;
   import org.apache.hadoop.mapreduce.*;
   import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
   import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
   import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
   import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.mahout.math.DenseVector;
+import org.apache.mahout.math.VectorWritable;
 
 public class anchorgraph {
     
-    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+    public static class Map extends Mapper<LongWritable, Text, Text, VectorWritable> {
+        private final static VectorWritable vec = new VectorWritable();
+        private Text vecID = new Text();
+        String vecSplitter = ",";
         
-        @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
             String line = value.toString();
-            StringTokenizer tokenizer = new StringTokenizer(line);
-            while(tokenizer.hasMoreTokens()){
-                word.set(tokenizer.nextToken());
-                context.write(word,one);   
-            }            
+            String[] vecArray = line.split(vecSplitter);
+            vecID.set(vecArray[0]);
+            int vecLength = vecArray.length;
+            
+           double dd[] = new double[vecLength-1];
+           
+           for(int j=1 ; j < vecLength; j++){
+               dd[j-1]=Double.parseDouble(vecArray[j]);    
+           }
+           
+           DenseVector actualVec = new DenseVector(dd);
+           vec.set(actualVec);
+           context.write(vecID, vec);
         }
     }
 
-    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable>{
-        
-        @Override
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();       
-            }
-            context.write(key, new IntWritable(sum));
-        }
-    }
+
 
     public static void main(String[] args) throws Exception{
         Configuration conf = new Configuration();
-        Job job = new Job(conf, "wordcount");
+        Job job = new Job(conf, "anchorgraph");
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(VectorWritable.class);
         
         job.setMapperClass(Map.class);
-        job.setReducerClass(Reduce.class);
         
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
